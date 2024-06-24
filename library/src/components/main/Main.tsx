d7b3d8cfef581600cd1004c9448fa8bff7b87aed
Julia Kocur <juliakocur@mail.com>
2024-06-24 22:21:07 +0200

    feat: add about section

--- library/src/components/main/Main.tsx
+++ library/src/components/main/Main.tsx
@@ -1,10 +1,12 @@
 import Welcome from "./welcome/Welcome";
+import About from "./about/About";
 
 const Main = () => {
     return (
         <main>
             <div className="container">
                 <Welcome />
+                <About />
             </div>
         </main>
     )
---
+++ library/src/components/main/about/About.tsx
@@ -0,0 +1,31 @@
+import './About.css';
+import image1 from '../../../assets/slider/image1.png';
+import image2 from '../../../assets/slider/image2.png';
+import image3 from '../../../assets/slider/image3.png';
+
+const About = () => {
+    return (
+        <section className="about">
+            <h2 className="section-header">About</h2>
+            <p className="about-text">The Brooklyn Library is a free workspace, a large number of books and a cozy coffee shop inside</p>
+            <div className="about-slider">
+                <img src={image1} alt="image1" />
+                <img src={image2} alt="image2" />
+                <img src={image3} alt="image3" />
+            </div>
+            <div className="pagination">
+                <div className="pagination-wrapper">
+                    <div className="pagination-button"></div>
+                </div>
+                <div className="pagination-wrapper">
+                    <div className="pagination-button"></div>
+                </div>
+                <div className="pagination-wrapper">
+                    <div className="pagination-button"></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
